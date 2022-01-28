fccac673ec1ebc8d960ea6f9e0f4712d5feea15c
Alexis-0 <66715451+Alexis-0@users.noreply.github.com>
2022-01-28 00:08:03 -0500

    Add files via upload

---
+++ Project 3/indexscript.js
@@ -0,0 +1,49 @@
+// "use.strict";
+
+function clearResult(){
+    document.getElementById("input-num").value = ''
+    document.getElementById("history").value = ''
+    
+  }
+  button.onclick = function() {
+    coinToss();
+ }
+function scoreIncrement() {
+     if (x==y){
+    score++;
+      document.getElementById("score").innerText="High-Score: " + score; 
+  }
+ }
+    
+      // random value generated
+      var y = Math.floor(Math.random() * 10 + 1);
+      
+      // counting the number of guesses
+      // made for correct Guess
+       var guess = 1;
+      
+      document.getElementById("check-btn").onclick = function(){
+      
+  // number guessed by user	
+  var x = document.getElementById("input-num").value;
+  
+  if(x == y)
+   {    
+       alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+               + guess + " GUESS ");
+   }
+   else if(x > y) /* if guessed number is greater
+                   than actual number*/ 
+   {    
+       guess++;
+       alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+   }
+   else
+   {
+       guess++;
+       alert("OOPS SORRY!! TRY A GREATER NUMBER")
+   }
+}
+ 				
+ 
+    
